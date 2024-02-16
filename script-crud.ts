f4d7d7a2b9d35efa4185a14c73e3ff0982d77a41
João Paulo Dagostin <dagostinjota@gmail.com>
2024-02-16 18:20:54 -0300

    Update script-crud.ts

--- script-crud.ts
+++ script-crud.ts
@@ -5,5 +5,23 @@ interface Tarefa {
 
 interface EstadoAplicacao {
   tarefas: Tarefa[];
-  tarefaSelecionada: Tarefa;
+  tarefaSelecionada: Tarefa | null;
 }
+
+let estado: EstadoAplicacao = {
+  tarefas: [
+    {
+      descricao: "Tarefa concluída",
+      concluida: true,
+    },
+    {
+      descricao: "Tarefa pendente 1",
+      concluida: false,
+    },
+    {
+      descricao: "Tarefa pendente 2",
+      concluida: false,
+    },
+  ],
+  tarefaSelecionada: null,
+};
