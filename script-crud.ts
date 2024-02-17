4ca9d7702962c77ba24ee7f5e521480b3a0353e6
João Paulo Dagostin <dagostinjota@gmail.com>
2024-02-17 20:17:31 -0300

    ts code
    new const

--- script-crud.ts
+++ script-crud.ts
@@ -25,3 +25,13 @@ let estado: EstadoAplicacao = {
   ],
   tarefaSelecionada: null,
 };
+
+const selecionarTarefa = (
+  estado: EstadoAplicacao,
+  tarefa: Tarefa
+): EstadoAplicacao => {
+  return {
+    ...estado,
+    tarefaSelecionada: tarefa === estado.tarefaSelecionada ? null : tarefa,
+  };
+};
